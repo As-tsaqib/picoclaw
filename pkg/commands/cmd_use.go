1a465e6582c71ad9b2fcd423a249b1292da10672
Assaqib <0xfiqks@gmail.com>
2026-08-21 00:16:31 +0800

    docs(commands): clarify normalized use forms

--- pkg/commands/cmd_use.go
+++ pkg/commands/cmd_use.go
@@ -3,7 +3,15 @@ package commands
 func useCommand() Definition {
 	return Definition{
 		Name:        "use",
-		Description: "Open the skill picker or force an installed skill for a request",
+		Description: "Pick, arm, clear, or force an installed skill",
 		Usage:       "/use [skill] [message]",
+		Category:    "Skills",
+		Examples: []string{
+			"/use",
+			"/use coding",
+			"/use coding explain this function",
+			"/use clear",
+			"/use off",
+		},
 	}
 }
